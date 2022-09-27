fce49d3ea2c1cdbfcfeb79ea12ca233bc0c4b112
heysourin <sourin07@gmail.com>
2022-09-27 21:00:19 +0530

    Contract deployed

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -13,8 +13,7 @@ async function main() {
   console.log(`${ToDoListDeploy}`);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
+
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
