01b261da64afbada9a0c74a356db14cc6db0544b
heysourin <sourin07@gmail.com>
2022-09-27 20:57:48 +0530

    Deployed to Goerli

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,24 +1,16 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
+const { ethers } = require("hardhat");
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const ToDoList = await hre.ethers.getContractFactory("ToDoList");
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  const ToDoListDeploy = await ToDoList.deploy();
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  await ToDoListDeploy.deployed();
 
-  await lock.deployed();
+  console.log(`Contract Deployed to: ${ToDoListDeploy.address}`);
 
-  console.log("Lock with 1 ETH deployed to:", lock.address);
+  console.log(`${ToDoListDeploy}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
