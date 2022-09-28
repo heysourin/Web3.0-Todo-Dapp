36ac8c6bd51d40db70ee8db468a9810261692b22
heysourin <sourin07@gmail.com>
2022-09-28 23:28:11 +0530

    Context updated

--- context/ToDolistApp.js
+++ context/ToDolistApp.js
@@ -4,14 +4,14 @@ import React, { useState, useEffect } from "react";
 import Web3Modal from "web3modal";
 import { ethers } from "ethers";
 
-//Internal import
+//Todo: Internal import
 import { toDoListAddress, toDoListABI } from "./constants";
 
 //? 'fetchContract' function will allow to communicate with smartcontract
 const fetchContract = (siggnerOrProvider) =>
   new ethers.Contract(toDoListAddress, toDoListABI, siggnerOrProvider);
 
-//as we are using react for data/state management
+//Todo: as we are using react for data/state management
 export const ToDoListContext = React.createContext();
 
 export const ToDoListProvider = ({ children }) => {
@@ -21,35 +21,35 @@ export const ToDoListProvider = ({ children }) => {
   const [myList, setMyList] = useState([]);
   const [allAddress, setAllAddress] = useState([]);
 
-  //----Connecting metamask---
+  //Todo: Checking if wallet is present in the browser or not----
   const checkIfWalletIsConnected = async () => {
     if (!window.ethereum) return setError("Please install metamask"); //Check metamask is present in the browser or not
-
     const account = await window.ethereum.request({ method: "eth_accounts" }); // Grabbing account
 
+    // In case of presense of multiple accounts, we will grab 1st account and use it.
     if (account.length) {
       setCurrentAccount(account[0]);
       console.log(account[0]);
     } else {
       setError("Please install metamask and reload");
-    } // In case of presense of multiple accounts, we will grab 1st account and use it.
+    }
   };
 
-  //----Connect Wallet---
+  //Todo: ---Connecting to the Wallet---
   const connectWallet = async () => {
-    //Doing the same check as above
+    //Doing the same checks as above
     if (!window.ethereum) return setError("Please install metamask");
 
     const account = await window.ethereum.request({
       method: "eth_requestAccounts",
-    });
-    //!Notice: method is a bit different here
+    });//!Notice: 'method: "eth_requestAccounts"'
 
     setCurrentAccount(account[0]);
   };
 
-  //---Interacting with smartcontract---
-  //this function will allow user to create todolist
+  //*---Interacting with smartcontract---
+
+  //TODO: This function will allow user to create 'todolist', grabbing the fucntion from smart contract
   const toDoList = async (message) => {
     try {
       //Connecting with smartcontract using web3modal
@@ -58,16 +58,73 @@ export const ToDoListProvider = ({ children }) => {
       const provider = new ethers.providers.Web3Provider(connection);
       const signer = provider.getSigner();
       const contract = await fetchContract(signer);
-
-      console.log(contract);
+      
+      const createList = await contract.createList(message);
+      createList.wait();
+      console.log(createList);
     } catch (err) {
       setError("Something wrong creating list");
+    }
+  };
+  
+  //TODO: This function will allow user to create getTodolist', grabbing the fucntion from smart contract
+  const getToDoList = async () => {
+    try {
+      const web3modal = new Web3Modal();
+      const connection = await web3modal.connect();
+      const provider = new ethers.providers.Web3Provider(connection);
+      const signer = provider.getSigner();
+      const contract = await fetchContract(signer);
+
+      //Getting the data
+      const getAllAddress = contract.getAddress();
+      setAllAddress(getAllAddress);//* Putting the data of the contract into this react component
+      console.log(getAllAddress);
+
+      getAllAddress.map(async (el) => {
+        const getSingleData = await contract.getCreatorData();
+        allToDoList.push(getAllAddress);
+        console.log(getSingleData);
+      });
+
+      const allMessage = await contract.getMessage();
+
+      setMyList(allMessage);
+    } catch (err) {
+      setError("Something wrong getting data");
+    }
+  };
+
+  //Todo: Change state of task (Toggle)
+  const change = async (address) => {
+    try {
+      const web3modal = new Web3Modal();
+      const connection = await web3modal.connect();
+      const provider = new ethers.providers.Web3Provider(connection);
+      const signer = provider.getSigner();
+      const contract = await fetchContract(signer);
+
+      const state = await contract.toggle(address);
+      state.wait();
+      console.log(state);
+    } catch (error) {
+      setError("Something wrong changing state");
     }
   };
 
   return (
     <ToDoListContext.Provider
-      value={{ checkIfWalletIsConnected, connectWallet, toDoList }}
+      value={{
+        checkIfWalletIsConnected,
+        connectWallet,
+        toDoList,
+        getToDoList,
+        change,
+        currentAccount,
+        error,
+        myList,
+        allAddress
+      }}
     >
       {children}
     </ToDoListContext.Provider>
