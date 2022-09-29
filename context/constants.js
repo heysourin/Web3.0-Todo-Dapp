9f3d20778242b15d078cd490ad28cd137e5e247c
heysourin <sourin07@gmail.com>
2022-09-29 23:59:59 +0530

    Ipdated

--- context/constants.js
+++ context/constants.js
@@ -1,5 +1,6 @@
 import ToDoList from "./ToDoList.json";
 
-export const toDoListAddress = "0x3F8b0E46a04Fc9F627C3294C975f752800B1E568";
+// export const toDoListAddress = "0x3F8b0E46a04Fc9F627C3294C975f752800B1E568";
+export const toDoListAddress = "0x55fd95F1e5dc7a8A77B116BC1Eb615aD1aadd7C2";
 
 export const toDoListABI = ToDoList.abi;
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -10,7 +10,7 @@ async function main() {
 
   console.log(`Contract Deployed to: ${ToDoListDeploy.address}`);
 
-  console.log(ToDoListDeploy);
+  // console.log(ToDoListDeploy);
 }
 
 
